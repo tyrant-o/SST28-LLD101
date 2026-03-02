c3e789632241122f29be69fb4dc4be6cb2c34bde
Ojas Sinha <150348895+tyrant-o@users.noreply.github.com>
2026-03-02 19:13:53 +0530

    Completed Ex1-6 renamed Demo0x to Main

--- SOLID/Ex1/src/Main.java
+++ SOLID/Ex1/src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Student Onboarding ===");
-        FakeDb db = new FakeDb();
-        OnboardingService svc = new OnboardingService(db);
+        Database db = new FakeDb();
+        StudentParser parser = new StudentParser();
+        ProgramList programList = new ProgramList();
+        programList.add("CSE");
+        StudentValidator validator = new StudentValidator(programList);
+        OnboardingService svc = new OnboardingService(db, parser, validator);
 
         String raw = "name=Riya;email=riya@sst.edu;phone=9876543210;program=CSE";
         svc.registerFromRawInput(raw);
 
-        System.out.println();
-        System.out.println("-- DB DUMP --");
-        System.out.print(TextTable.render3(db));
+        String raw2 = "name=Vikram;email=vikram@sst.edu;phone=9876543211;program=IT";
+        svc.registerFromRawInput(raw2);
     }
-}
+}  
