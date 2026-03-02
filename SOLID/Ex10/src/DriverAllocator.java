6d12737d951f387b29df57a5c721d7ae034280af
Ojas Sinha <150348895+tyrant-o@users.noreply.github.com>
2026-03-02 19:31:41 +0530

    Completed Ex10

--- SOLID/Ex10/src/DriverAllocator.java
+++ SOLID/Ex10/src/DriverAllocator.java
@@ -1,6 +1,3 @@
-public class DriverAllocator {
-    public String allocate(String studentId) {
-        // fake deterministic driver
-        return "DRV-17";
-    }
+public interface DriverAllocator {
+    String allocate(String studentId);
 }
